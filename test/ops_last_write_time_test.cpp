7717a0a646908ee21f76b76b29446391f624d895
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2021-11-27 00:20:31 +0000

    test: take into account file times granularity

--- test/ops_last_write_time_test.cpp
+++ test/ops_last_write_time_test.cpp
@@ -3,10 +3,11 @@
 //    (See accompanying file LICENSE or copy at
 //   https://opensource.org/licenses/BSD-3-Clause)
 
+#include <thread>
 #if defined(__clang__)
 #pragma clang diagnostic push
 // Catch2 uses a lot of macro names that will make clang go crazy
-#if !defined(__APPLE__)
+#if (__clang_major__ >= 13) && !defined(__APPLE__)
 #pragma clang diagnostic ignored "-Wreserved-identifier"
 #endif
 // Big mess created because of the way spdlog is organizing its source code
@@ -16,7 +17,7 @@
 // with clang (rightfully) complaining that the template definitions are not
 // available when the template needs to be instantiated here.
 #pragma clang diagnostic ignored "-Wundefined-func-template"
-#endif  // __clang__
+#endif // __clang__
 
 #include <fcntl.h> /* Definition of AT_* constants */
 #include <sys/stat.h>
@@ -30,12 +31,22 @@ using testing::ComparePaths;
 
 using time_type = fs::file_time_type;
 
+namespace {
+auto approx_equal(time_type file_time, time_type expected) -> bool {
+  const auto FILE_TIME_GRANULARITY_SECONDS = 2;
+  auto delta = expected - file_time;
+  if (delta.count() < 0) {
+    delta = -delta;
+  }
+  return delta < std::chrono::seconds(FILE_TIME_GRANULARITY_SECONDS);
+}
+} // namespace
+
 // -----------------------------------------------------------------------------
 //  last_write_time
 // -----------------------------------------------------------------------------
 
-TEST_CASE("Ops / last_write_time / read",
-          "[common][filesystem][ops][last_write_time]") {
+TEST_CASE("Ops / last_write_time / read", "[common][filesystem][ops][last_write_time]") {
   // read times
 
   auto p = testing::nonexistent_path();
@@ -44,8 +55,7 @@ TEST_CASE("Ops / last_write_time / read",
   REQUIRE(ec);
   REQUIRE(ec == std::make_error_code(std::errc::no_such_file_or_directory));
 
-  REQUIRE_THROWS_MATCHES(
-      last_write_time(p), fs::filesystem_error,
+  REQUIRE_THROWS_MATCHES(last_write_time(p), fs::filesystem_error,
       testing::FilesystemErrorDetail(
           std::make_error_code(std::errc::no_such_file_or_directory), p));
 
@@ -53,23 +63,13 @@ TEST_CASE("Ops / last_write_time / read",
   REQUIRE(exists(p));
   mtime = last_write_time(p, ec);
   REQUIRE(!ec);
-  REQUIRE(mtime <= time_type::clock::now());
+  // Various implementations delay updates of file stats or have high time granularity for file
+  // times making it hard to reliably test the value.
+  REQUIRE(approx_equal(mtime, time_type::clock::now()));
   REQUIRE(mtime == last_write_time(p));
 }
 
-namespace {
-auto approx_equal(time_type file_time, time_type expected) -> bool {
-  auto delta = expected - file_time;
-  if (delta <
-      std::chrono::duration<std::int64_t, std::ratio<1, 1000000000>>::zero()) {
-    delta = -delta;
-  }
-  return delta < std::chrono::seconds(1);
-}
-}  // namespace
-
-TEST_CASE("Ops / last_write_time / write",
-          "[common][filesystem][ops][last_write_time]") {
+TEST_CASE("Ops / last_write_time / write", "[common][filesystem][ops][last_write_time]") {
   // write times
   auto p = testing::nonexistent_path();
 
@@ -105,10 +105,10 @@ TEST_CASE("Ops / last_write_time / write",
   ec = bad_ec;
   time -= std::chrono::milliseconds(1000 * 60 * 10 + 15);
   last_write_time(f.path_, time, ec);
-  REQUIRE(ec);  // negative seconds since epoch is invalid
+  REQUIRE(ec); // negative seconds since epoch is invalid
   REQUIRE(approx_equal(last_write_time(f.path_), time_type()));
 }
 
 #if defined(__clang__)
 #pragma clang diagnostic pop
-#endif  // __clang__
+#endif // __clang__
